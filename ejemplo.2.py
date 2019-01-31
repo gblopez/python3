06a463122059d2bc17bcdceb1779be5e632d31b2
gblopez <lopezblopez260@gmail.com>
2019-01-30 21:54:03 -0600

    dolar a quetzles

--- ejemplo.2.py
+++ ejemplo.2.py
@@ -6,7 +6,7 @@ FEC_ACT = 2019
 fec_nac = int(input("ingres tu año de nacimiento"))
 
 edad = FAC_ACT - fec_nac
-if edad >= 18:
+if edad >= 18: 
     print("Eres mayor de edad")
 else:
     print("Eres menor de edad")
---
+++ ejercicio 1.py
@@ -0,0 +1,27 @@
+##Gerson Lopez
+1
+##nesesitan crear un programa que les permita seleccionar entre 1 o 2 opciones primera
+##combertir quetzles a dolares o dolares quetzales si escoge una opcion invalida entra que decir
+##que decirle "opcion incorrec
+
+
+
+
+
+pdolar=float(input("ingrese el precio actual del dolar: ")) 
+dolares=float(input("ingrese la cantidad de dolares a convertir: "))
+
+quetzales=dolares*pdolar
+print(dolares,"dolares equivalen a ",quetzales,"pesos")
+
+quetzales=float(input("ingrese el precio actual de quetzales: ")) 
+quetzal=float(input("ingrese la cantidad de quetzales a convertir: "))
+
+dolares=quetzal*quetzales
+print(quetzales,"quetzales equivalente a ",dolares,"dolar")
+
+
+
+
+
+
