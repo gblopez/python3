6764e1e9d990f31a7a4c1669d4cea45dc760c4ac
gblopez <lopezblopez260@gmail.com>
2019-01-30 22:31:53 -0600

    Ejercicio3

---
+++ Ejercicio 3.py
@@ -0,0 +1,42 @@
+
+##Gerson lopez 5TO BIPC
+2
+##solicitiar al usuario que escoja uno de las opciones 1 sumar 2 numeros
+##2 restar tres numeros  3 multiplicar cuatro numeros 4 dividir 2 numeros
+##si el usuario ingrese una opcion invalidad hacersela saver
+
+
+N1=int(input("numero1: "))
+N2=int(input("numero2: "))
+print("El resultado es: "+ str(N1+N2))
+
+
+
+
+
+r1=int(input("#uno"))
+r2=int(input("#dos"))
+r3=int(input("#tres"))
+total= -r1-r2-r3
+print("el total de la respta es de: ",total)
+
+
+
+
+
+m1=int(input("#1"))
+m2=int(input("#2"))
+m3=int(input("#3"))
+m4=int(input("#4"))
+totalM=m1*m2*m3*m4
+print("el total de multiplicacion es:",totalM)
+
+
+
+
+
+n1=int(input("n1: "))
+n2=int(input("n2: "))
+total=n1/n2
+print("el total es ",total)
+
